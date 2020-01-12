ace83dbf21fc27d1a88cef3b670f7d58f54834d7
siscia <simone@redbeardlab.com>
2020-01-12 09:36:43 +0000

    add option to pass arbitrary arguments

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-docker_run="docker run -d -p 6379:6379 redis:$INPUT_REDIS_VERSION redis-server --databases $INPUT_NUMBER_OF_DATABASES"
+docker_run="docker run -d -p 6379:6379 redis:$INPUT_REDIS_VERSION \
+        redis-server --databases $INPUT_NUMBER_OF_DATABASES \
+        $INPUT_SERVER_ARGUMENTS"
 
 sh -c "$docker_run"
